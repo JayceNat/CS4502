9a01743f63167eb325902dc264b1d7aaf51a361c
JacksonMarkowski <markowski.jackson@gmail.com>
2017-04-09 14:05:27 -0600

    Visual graphs of crimes by hour

--- visual_data/DenverCrimeHourly.py
+++ visual_data/DenverCrimeHourly.py
@@ -3,15 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import QueryDB as qry
 
+offense = ["1102", "Sexual Assault - Rape", "SexualAssaultRape"]
+
 def main(argv):
 	setPlotDesign()
 	data = qry.queryDB("select Date_format(First_Occurrence_Date, '%H') as 'Hour', Count(1) as 'Crime_Count' from DenverCrime where Is_Crime = 1 group by Date_format(First_Occurrence_Date, '%H');")
 	queryToPlot(data, "#6DA2BE", 0, "All Crimes")
-	data = qry.queryDB("select Date_format(First_Occurrence_Date, '%H') as 'Hour', Count(1) as 'Crime_Count' from DenverCrime where Is_Crime = 1 and Offense_Code = 1102 group by Date_format(First_Occurrence_Date, '%H');")
-	queryToPlot(data, "#C16256", .3, "Sexual Assault - rape")
+	data = qry.queryDB("select Date_format(First_Occurrence_Date, '%H') as 'Hour', Count(1) as 'Crime_Count' from DenverCrime where Is_Crime = 1 and Offense_Code = " + offense[0] + " group by Date_format(First_Occurrence_Date, '%H');")
+	#plt.subplot(112)
+	queryToPlot(data, "#C16F65", .3, offense[1])
 
 	plt.legend(loc="upper center", frameon=False, fontsize=12)
-	plt.show()
+	#plt.show()
+	plt.savefig("CrimeHourlyBarGraphs/AllCrimes-" + offense[2] + "-percent.png", bbox_inches="tight", dpi=300)
 
 def queryToPlot(data, color, offset, label):
 	hourList = []
@@ -21,7 +25,7 @@ def queryToPlot(data, color, offset, label):
 			hourList.append(float(hour)+offset)
 			crimeCountList.append(float(crimeCount))
 
-	plot(hourList, normalize(crimeCountList), color, offset, label)
+	plot(hourList, percentage(crimeCountList), color, offset, label)
 
 def percentage(data):
 	sum = 0
@@ -51,12 +55,12 @@ def setPlotDesign():
 	ax.spines["right"].set_visible(False)
 
 	plt.xlabel("Time of Day")
-	plt.ylabel("Offenses Normalized")
+	plt.ylabel("% of Occurrence")
 
 	plt.tick_params(bottom='off', top='off', right='off', left='off')
 
 	plt.xticks(range(0, 24, 6), ["12 am", "6 am", "12 pm", "6 pm"], rotation=30, fontsize=14)
-	plt.yticks([0, .25, .5, .75, 1], fontsize=14)
+	plt.yticks(fontsize=14)
 
 def plot(x, y, colorValue, offset, label):
 	plt.bar(x, y, width=.3, color=colorValue, label=label, edgecolor="none")
